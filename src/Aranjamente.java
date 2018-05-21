dc95cb31eac8b2aca45a896e3a029b3dfa7c7308
Roman Gabriel-Marian <romangmarian@gmail.com>
2018-05-21 21:13:19 +0300

    Somehow it works

--- src/Aranjamente.java
+++ src/Aranjamente.java
@@ -12,28 +12,23 @@ import java.util.List;
 
 import static java.sql.DriverManager.println;
 
-public class Combinari implements Factorial {
+public class Aranjamente implements Factorial {
 
     public void solve(int n, int k) {
 
-        Model model = new Model("Combinari");
-        IntVar[] combs = new IntVar[k];
-        for (int i = 0; i < k; i++)
-            combs[i] = model.intVar("P" + i, 1, n);
-        model.allDifferent(combs).post();
-        println("before for");
-        for(int i  = 0; i < k-1; i++)
-            for(int j = i + 1; j < k; j++) {
-                model.arithm(combs[i], "<", combs[j]).post();
-            }
+        Model model = new Model("Aranjamente");
+        IntVar[] aranjs = new IntVar[k];
+        for(int i = 0; i < k; i++)
+            aranjs[i] = model.intVar("P" + i, 1, n);
+        model.allDifferent(aranjs).post();
         System.out.println(model.getName());
         Solver solver = model.getSolver();
-        Criterion solcpt = new SolutionCounter(model, factorial(n) / (factorial(n - k) * factorial(k)));
+        Criterion solcpt = new SolutionCounter(model, factorial(n)/factorial(n - k));
         List<Solution> solutions = solver.findAllSolutions(solcpt);
         try {
-            PrintWriter writer = new PrintWriter("combinari.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("aranjamente.txt", "UTF-8");
             writer.println(solutions.size());
-            for (Solution s : solutions)
+            for(Solution s: solutions)
                 writer.println(s);
             writer.close();
         } catch (FileNotFoundException e) {
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String args[]){
         Permutari p = new Permutari();
         p.solve(4);
+        Aranjamente a = new Aranjamente();
+        a.solve(3,2);
+        Combinari c = new Combinari();
+        c.solve(4, 2);
     }
 }
